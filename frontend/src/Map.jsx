80be2e9b0fc30a109caa52d0b393916ce668de00
kamronarabi <kamronarabi@gmail.com>
2025-05-25 18:00:39 +0200

    UImockup+map

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 const Map = () => {
   return (
-    <div>Map</div>
+     <MapContainer
+      center={[-33.9, 19.7]}
+      zoom={7}                 
+      scrollWheelZoom          
+      style={{
+        height: "100%",        
+        width: "100%"
+      }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+    </MapContainer>
   )
 }
 
