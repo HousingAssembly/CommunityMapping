d14074d3cffe643fb1df7090af165645def78a17
Amit Gupta <sgupta2435@gmail.com>
2025-05-28 06:49:33 -0400

    added some small zoom in code as base

--- frontend/src/Map.jsx
+++ frontend/src/Map.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Circle, useMapEvent } from 'react-leaflet'
+
+function ZoomToDistrict(){
+  // What does the function need to do
+  // Get mouse click location
+  // Check which district it's in
+  // Given that, setView with a specific latlong at the center and then zoom in maybe 11 or 12
+  // Then, populate the pins for the areas
+  const map = useMapEvent({
+    click() {
+      map.setZoom(12)
+    },
+  })
+  return null;
+}
 
 const Map = () => {
   return (
@@ -16,6 +30,10 @@ const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
+
+      <Circle center={[-34.040278, 18.677778]} pathOptions={{fillColor: 'red'}} radius={3000} />
+
+    <ZoomToDistrict />
     </MapContainer>
   )
 }
