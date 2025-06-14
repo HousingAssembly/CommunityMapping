bd69ab0341ff98cfaa3500dae2e7d7d8792ff21d
Kamron Arabi <kamronarabi@gmail.com>
2025-06-14 21:11:27 +0200

    Marker fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,24 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import L from 'leaflet';
+
+import L from "leaflet";
+
+import iconRetina   from "leaflet/dist/images/marker-icon-2x.png";
+import iconNormal   from "leaflet/dist/images/marker-icon.png";
+import iconShadow   from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetina,
+  iconUrl:       iconNormal,
+  shadowUrl:     iconShadow,
+});
 
 createRoot(document.getElementById('root')).render(
+
     <BrowserRouter>
       <AdminProvider>
         <ChakraProvider value={defaultSystem}>
